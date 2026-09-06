2aa2d7430ece11b7cfcdc0e37d3dd36e8895089a
Anand Raj <anandraj24092002@gmail.com>
2026-09-07 02:09:23 +0530

    initial  commit

---
+++ config-server/src/test/java/com/smart/event/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart.event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
